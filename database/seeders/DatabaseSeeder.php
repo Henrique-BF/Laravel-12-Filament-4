0a8c7b8c6f8449321383595b7b564add9cc29632
Henrique <henrybf10@hotmail.com>
2025-08-27 17:48:32 -0300

    Add Users

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +14,25 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::factory()->create([
+            'name' => 'Test 1',
+            'email' => 'test@test.com',
+            'role' => 'admin',
+            'password' => Hash::make('15121998'),
+        ]);
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Test 2',
+            'email' => 'test_2@test.com',
+            'role' => 'almox',
+            'password' => Hash::make('15121998'),
+        ]);
+
+        User::factory()->create([
+            'name' => 'Test 3',
+            'email' => 'test_3@test.com',
+            'role' => 'vendas',
+            'password' => Hash::make('15121998'),
         ]);
     }
 }
